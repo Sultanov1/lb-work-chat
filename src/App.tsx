a019fa39f6794aa36ae6bc45a637024b5207d6c2
Oybek Sultanov <oibekzzz09@gmail.com>
2024-06-21 10:05:10 +0600

    Делаем post запрос на нашу апи с помощью post запроса отправляем наши значения.Также благодаря этому реализовали блок формы.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
+import React from 'react';
 import ChatForm from './components/ChatForm/ChatForm';
 import ChatList from './components/ChatList/ChatList';
-import {useState} from 'react';
 
-const App = () => (
+const App = () => {
+  const sendMessages = async (message: string, author: string) => {
+    try {
+      const data = new URLSearchParams();
+      data.set('author', author);
+      data.set('message', message);
+      await fetch(' http://146.185.154.90:8000/messages', {
+        method: 'POST',
+        body: data,
+      });
+    } catch (err) {
+      console.log('Something went wrong', err);
+    }
+  };
 
-  <div className="text-center">
-    <ChatForm/>
-    <ChatList/>
-  </div>
-);
+  return (
+    <div className="text-center">
+      <ChatForm onSendMessage={sendMessages}/>
+      <ChatList/>
+    </div>
+  );
+};
 
 export default App;
--- src/components/ChatForm/ChatForm.tsx
+++ src/components/ChatForm/ChatForm.tsx
@@ -1,17 +1,39 @@
-const ChatForm = () => {
+import React, {useState} from 'react';
+
+interface Props {
+  onSendMessage: (message: string, author: string) => void;
+}
+
+const ChatForm: React.FC<Props> = ({onSendMessage}) => {
+  const [message, setMessage] = useState('');
+  const [author, setAuthor] = useState('');
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (message && author) {
+      onSendMessage (message, author);
+      setMessage('');
+      setAuthor('');
+    }
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <label>
         Enter your name
         <input
           type="text"
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
           required/>
       </label>
       <label>
         Type your message
         <input
           type="text"
-        required/>
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          required/>
       </label>
       <button type="submit">Send</button>
     </form>
